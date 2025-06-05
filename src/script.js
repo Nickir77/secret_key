c0a26eeb0844458855dd36f18c94325e2c4e4e72
Nickir77 <46559531+Nickir77@users.noreply.github.com>
2025-06-05 06:10:27 +0300

    Create script.js

---
+++ src/script.js
@@ -0,0 +1,19 @@
+const PRIME = 13;
+const BASE = 6;
+const MY_SECRET = getRandomInt(PRIME - 1);
+const MY_KEY = (BASE ** MY_SECRET) % PRIME;
+
+//console.log(MY_KEY);
+
+document.getElementById("button").addEventListener('click', function () {
+    var their_key = Number(document.getElementById('text_fin').value);
+    var secret_text = document.getElementById('text_secret');
+    secret_text.value = "Ваш секретный ключ это " + ((their_key ** MY_KEY) % PRIME);
+});
+
+function getRandomInt(max) {
+  return (Math.floor(Math.random() * max) + Date.now() % max) % max;
+}
+
+var init_text = document.getElementById("text_init");
+init_text.value = "Покажите игроку число " + MY_KEY;
